0771e6c6cdb11b262346500b168fa18099ea59c7
Ahmed Roshdy Ragheb Mohamed <ahmedroshdy6555554@gmail.com>
2024-12-21 21:40:12 +0200

    Adding comments to the code

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
-import 'dart:io';
+
 void main() {
 
-  List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// Loop contain the numbers we will check
 
-  for (int number in numbers) {
+  for (int number in numbers) { // switch-case to determine if the number is even or odd
 
     switch (number % 2) {
       case 0:
@@ -13,7 +13,7 @@ void main() {
         print('$number is odd');
         break;
       default:
-        print('Unexpected case for $number');
+        print('Unexpected case for $number'); // in case we added anything in the list except number
     }
   }
 }
